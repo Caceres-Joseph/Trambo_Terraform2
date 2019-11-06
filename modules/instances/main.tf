1c95aafaf2b1a2ab1d6cd1cc73b8da55d7e4f590
Joseph <josephccaceres@gmail.com>
2019-11-06 17:20:13 -0600

    cambios

--- modules/instances/main.tf
+++ modules/instances/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ec2_joseph" {
   instance_type               = "t2.micro"
   //availability_zone           = "${var.availability_zones.*.id[0]}"  
   monitoring                  = true
-  subnet_id                   = "${var.public_subnet_ids.*.id[0]}"
+  subnet_id                   = "${var.public_subnet_ids.id[0]}"
   associate_public_ip_address = true
 
   tags = {
