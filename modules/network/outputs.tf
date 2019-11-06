0544fe4ba3941dfaa4ec28a1a5041a8162471fc1
Joseph <josephccaceres@gmail.com>
2019-11-06 17:09:45 -0600

    list

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,13 +1,16 @@
 
 output "public_subnet_ids" {
+  type    = "list"
   value = ["${aws_subnet.public.*.id}"]
 }
 
 output "private_subnet_ids" {
+  type    = "list"
   value = ["${aws_subnet.private.*.id}"]
 }
 
 
 output "availability_zones" {
+  type    = "list"
   value = ["${var.availability_zones}"]
 }
