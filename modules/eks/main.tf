6c1a36fd2135aa36357405b857925a837c4e2883
Joseph <josephccaceres@gmail.com>
2019-11-07 18:13:38 -0600

    role

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -72,17 +72,19 @@ resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
 
 resource "aws_eks_cluster" "demo" {
   name            = "${var.cluster-name}"
-  role_arn        = "${aws_iam_role.demo-cluster.arn}"
+  //role_arn        = "${aws_iam_role.demo-cluster.arn}"
 
   vpc_config {
     security_group_ids = ["${aws_security_group.demo-cluster.id}"]
     subnet_ids         = "${var.public_subnet_ids.*.id}"
   }
 
+/*
   depends_on = [
     "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
     "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy",
   ]
+  */
 }
 
 
